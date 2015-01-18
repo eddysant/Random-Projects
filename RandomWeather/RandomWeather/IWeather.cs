05d6b69dab26f95efb77da3c91f50c1b048a7f3c
Eddy <eddysant@gmail.com>
2015-01-18 16:26:31 -0500

    Random Weather Server
    Simple server that returns random weather results used for testing
    another app.

---
+++ RandomWeather/RandomWeather/IWeather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RandomWeather
+{	
+	[ServiceContract]
+	public interface IWeather
+	{
+		[WebGet()]
+		[OperationContract]
+		WeatherObj GetWeather();
+	}
+
+	[DataContract]
+	public class WeatherObj
+	{
+		[DataMember]
+		public int Temperature { get; set; }
+		[DataMember]
+		public string Condition { get; set; }
+	}
+}
---
+++ RandomWeather/RandomWeather/Weather.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RandomWeather
+{
+	public class Weather : IWeather
+	{
+		public WeatherObj GetWeather()
+		{
+			var random = new Random();
+
+			int temp = random.Next(-100, 100);
+			
+			int max = 6;
+			if (temp < 10)
+				max = 5;
+
+			int weather = random.Next(max);
+			string weatherString = string.Empty;
+
+			switch (weather)
+			{
+				case 0:
+					weatherString = "Partly Cloudy";
+					break;
+				case 1:
+					weatherString = "Thunderstorms";
+					break;
+				case 2:
+					weatherString = "Raining";
+					break;
+				case 3:
+					weatherString = "Sunny";
+					break;
+				case 4:
+					weatherString = "Cloudy";
+					break;
+				case 5:
+					weatherString = "Snowing";
+					break;
+			}
+						
+
+			WeatherObj weatherObj = new WeatherObj();
+			weatherObj.Temperature = temp;
+			weatherObj.Condition = weatherString;
+			return weatherObj;
+		}
+	}
+}
